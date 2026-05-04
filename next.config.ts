16eb6a1a3fb5b3d68001ad55bb60a9b588e458e1
vincentchen <a978090@gmail.com>
2026-05-05 03:23:17 +0800

    關閉nextjs圖片自動轉換&將部分簡體中文文本轉換為繁體中文

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  images: {
-    minimumCacheTTL: 2678400,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.supabase.co',
-      },
-    ],
-  },
+    images: {
+        minimumCacheTTL: 2678400,
+        unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**.supabase.co',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
